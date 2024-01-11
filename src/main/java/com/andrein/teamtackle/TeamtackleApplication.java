0c25482afdad837df41f23a60771b8733c3fd358
andrei-n93 <andrei.n93n@gmail.com>
2024-01-11 22:34:10 +0200

    Initial commit

---
+++ src/main/java/com/andrein/teamtackle/TeamtackleApplication.java
@@ -0,0 +1,13 @@
+package com.andrein.teamtackle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamtackleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamtackleApplication.class, args);
+	}
+
+}
